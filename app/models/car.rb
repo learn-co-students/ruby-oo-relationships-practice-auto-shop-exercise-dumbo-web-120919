50985a5a5b50d9b863be73e0eadbb151426d0dac
Annie Liao <liaowow@gmail.com>
2019-12-15 22:00:51 -0500

    Done.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,31 @@
 class Car
+  attr_accessor :car_owner, :mechanic
+  attr_reader :make, :model, :classification
 
-  attr_reader :make, :model
+  @@all = []
 
-  def initialize(make, model, classification)
+  def initialize(make, model, classification, car_owner, mechanic)
     @make = make
     @model = model
+    @car_owner = car_owner
+    @mechanic = mechanic
     @classification = classification
+    @@all << self
   end
 
+  def self.car_classifications
+    @@all.map {|car_instance| car_instance.classification}
+  end
+
+  def self.matching_mechanics(classification)
+    @@all.select.map do |car_instance|
+      if car_instance.classification == classification
+        car_instance.mechanic
+      end
+    end
+  end
+
+  def self.all
+    @@all
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,24 @@ def reload
   load 'config/environment.rb'
 end
 
+# CarOwner.new(name)
+co1 = CarOwner.new("Owner 1")
+co2 = CarOwner.new("Owner 2")
+co3 = CarOwner.new("Owner 3")
+co4 = CarOwner.new("Owner 4")
+
+# Mechanic.new(name, specialty)
+m1 = Mechanic.new("Mechanic 1", "Compact")
+m2 = Mechanic.new("Mechanic 2", "Van")
+m3 = Mechanic.new("Mechanic 3", "Van")
+m4 = Mechanic.new("Mechanic 4", "Truck")
+
+# Car.new(make, model, classification, car_owner, mechanic)
+c1 = Car.new("Toyota", "Camry", "Compact", co1, m1)
+c2 = Car.new("Ford", "Transit", "Van", co2, m2)
+c3 = Car.new("GMC", "Savana", "Van", co1, m2)
+c4 = Car.new("Chevy", "Silverado", "Truck", co4, m4)
+
 binding.pry
+puts "vrooooom"
+# CarOwner -< Car >- Mechanic

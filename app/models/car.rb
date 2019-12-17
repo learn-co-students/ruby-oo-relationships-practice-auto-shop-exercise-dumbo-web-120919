273141684ee79501fc6680d3d2184706b4163014
hylobates-lar <alison.v.quaglia@gmail.com>
2019-12-17 16:04:22 -0500

    all tests passing

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,38 @@
 class Car
 
-  attr_reader :make, :model
+  attr_reader :make, :model, :classification 
+  attr_accessor :owner, :mechanic 
 
-  def initialize(make, model, classification)
+  @@all = []
+
+  def initialize(make, model, classification, owner, mechanic)
     @make = make
     @model = model
     @classification = classification
+    @owner = owner
+    @mechanic = mechanic 
+
+    @@all << self
+  end
+
+  def self.all 
+    @@all
+  end
+
+  def self.classifications
+    cars = self.all.map do |car|
+      car.classification
+    end
+    cars.uniq 
+  end
+
+  def matching_mechanic
+    Mechanic.all.select do |mechanic|
+      if mechanic.specialty == self.classification
+        return mechanic 
+      end
+    end
   end
 
 end
+
--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -2,9 +2,37 @@ class CarOwner
 
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+
+    @@all << self 
   end
 
+  def self.all 
+    @@all 
+  end
+
+  def all_cars
+    Car.all.select do |car|
+      car.owner == self 
+    end
+  end
+
+  def all_mechanics
+    mechanics = self.all_cars.map do |car|
+      car.mechanic
+    end
+    mechanics.uniq 
+  end
+
+  def self.average_cars
+    owners = Car.all.map do |car|
+      car.owner 
+    end
+    Car.all.length / owners.uniq.length
+  end
 
 end
+
